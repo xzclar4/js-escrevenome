289c73f0a5c95fcb5571858e0a14ee27cfb1d4f4
xzclar4 <anaclarathavares2041@gmail.com>
2024-05-23 21:35:05 -0300

    Update Sketch.js
    editado

--- Sketch.js
+++ Sketch.js
@@ -2,12 +2,15 @@ function setup() {
     createCanvas(700, 700);
     background ("white")
   }
+
   function draw(){
     
     stroke("blue");
     fill("red");
-    
+
+
+      
     if ( mouseIsPressed) {
     rect( mouseX,mouseY, 20,35);
     }
-    }
+  }
